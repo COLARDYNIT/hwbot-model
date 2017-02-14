9828ae21db66611ce1f1eebbeeb7e45361ae9c96
sthaes <st.haesendonck@gmail.com>
2017-02-14 13:31:57 +0100

    added basic result need for check on delete contest,gpu and cpu

---
+++ src/main/java/be/colardyn_it/model/Result.java
@@ -0,0 +1,86 @@
+package be.colardyn_it.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * Created by stijnhaesendonck on 14/02/2017.
+ */
+@Entity
+@Table(name = "result")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Result {
+    private Integer resultId;
+    private Integer contestId;
+    private Integer cpuId;
+    private Integer gpuId;
+    private Integer cpuFamilyId;
+    private Integer gpuFamilyId;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "RESULT_ID", unique = true, nullable = false)
+    public Integer getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(Integer resultId) {
+        this.resultId = resultId;
+    }
+    @Column(name = "CONTEST_ID")
+    @NotFound(action = NotFoundAction.IGNORE)
+    public Integer getContestId() {
+        return contestId;
+    }
+
+    public void setContestId(Integer contestId) {
+        this.contestId = contestId;
+    }
+    @Column(name = "CPU_ID")
+    @NotFound(action = NotFoundAction.IGNORE)
+    public Integer getCpuId() {
+        return cpuId;
+    }
+
+    public void setCpuId(Integer cpuId) {
+        this.cpuId = cpuId;
+    }
+    @Column(name = "GPU_ID")
+    @NotFound(action = NotFoundAction.IGNORE)
+    public Integer getGpuId() {
+        return gpuId;
+    }
+
+    public void setGpuId(Integer gpuId) {
+        this.gpuId = gpuId;
+    }
+    @Column(name = "CPUFAMILY_ID")
+    @NotFound(action = NotFoundAction.IGNORE)
+    public Integer getCpuFamilyId() {
+        return cpuFamilyId;
+    }
+
+    public void setCpuFamilyId(Integer cpuFamilyId) {
+        this.cpuFamilyId = cpuFamilyId;
+    }
+    @Column(name = "GPUFAMILY_ID")
+    @NotFound(action = NotFoundAction.IGNORE)
+    public Integer getGpuFamilyId() {
+        return gpuFamilyId;
+    }
+
+    public void setGpuFamilyId(Integer gpuFamilyId) {
+        this.gpuFamilyId = gpuFamilyId;
+    }
+}
